23dd37ba2a366186f696e54ee2b620305b084460
ryota-murakami <dojce1048@gmail.com>
2022-06-16 00:24:38 +0900

    start Tweets

--- src/pages/ProfileModal/Tweets/index.tsx
+++ src/pages/ProfileModal/Tweets/index.tsx
@@ -1,9 +1,9 @@
 import Box from '@mui/material/Box'
 import React from 'react'
 
-import { palette } from '../../MuiTheme'
+import { palette } from '../../../MuiTheme'
 
-const Tweets: React.FC = () => {
+const Index: React.FC = () => {
   return (
     <Box /* eslint-disable-next-line prettier/prettier */
       sx={{ backgroundColor: palette.wedget.background, border: `2px solid ${palette.border.dark}`, height: 670,marginLeft: '10px', width: 595, }}>
@@ -12,4 +12,4 @@ const Tweets: React.FC = () => {
   )
 }
 
-export default Tweets
+export default Index
