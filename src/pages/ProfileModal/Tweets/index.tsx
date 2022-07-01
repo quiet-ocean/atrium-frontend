a5abb8eaf04b988d0cbd8d5c1e4fec2956be4eb0
chris bishop <ychrisbishop@gmail.com>
2022-07-02 00:11:46 +0900

    Update Tweet component

--- src/pages/ProfileModal/Tweets/index.tsx
+++ src/pages/ProfileModal/Tweets/index.tsx
@@ -1,6 +1,7 @@
 import Box from '@mui/material/Box'
-import Text from '@mui/material/Typography'
+// import Text from '@mui/material/Typography'
 import React from 'react'
+import { FractulAltText } from '../../../components/Styled'
 
 import { palette } from '../../../MuiTheme'
 
@@ -10,9 +11,9 @@ const Index: React.FC = () => {
   return (
     <Box /* eslint-disable-next-line prettier/prettier */
       sx={{ backgroundColor: palette.widget.background, border: `2px solid ${palette.border.dark}`, display: 'flex',flexDirection: 'column', gap: '10px', height: 670, marginLeft: '10px', paddingLeft: '35px', paddingTop: '20px', width: 595 }}>
-      <Text variant="h4" sx={{ color: '#fff', fontWeight: 'bold' }}>
+      <FractulAltText variant="h4" sx={{ color: '#fff', fontWeight: 'bold' }}>
         Tweets
-      </Text>
+      </FractulAltText>
       <Stream />
     </Box>
   )
