e89f71bbcb4f0f0acf557fbc48dc8a3ad35becb2
chris bishop <ychrisbishop@gmail.com>
2022-07-22 22:36:08 +0100

    Fetch nfts by user wallet

--- src/pages/SetAvatar.tsx
+++ src/pages/SetAvatar.tsx
@@ -62,7 +62,7 @@ const SetAvatar = () => {
     accountId = 'swiftyyy.near'
     let parasApiUrl =
       process.env.VITE_PARAS_API_URL || 'https://api-v2-mainnet.paras.id'
-    fetch(`${parasApiUrl}/token?owner_id=${accountId}&__limit=100`)
+    fetch(`${parasApiUrl}/token?owner_id=${accountId}&__limit=10`)
       // fetch("https://api-v2-mainnet.paras.id/token?owner_id=swiftyyy.near")
       .then(async (res) => {
         let result = await res.json()
