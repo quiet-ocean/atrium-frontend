a27ab179039def749b78e79f6a70804229cf573f
chris bishop <ychrisbishop@gmail.com>
2022-08-04 06:23:15 +0200

    Update LoginSubLayout.tsx

--- src/pages/Login/LoginSubLayout.tsx
+++ src/pages/Login/LoginSubLayout.tsx
@@ -1,10 +1,10 @@
-import { Box, Typography } from '@mui/material'
 import ArrowBackIcon from '@mui/icons-material/ArrowBack'
 import ArrowForwardIcon from '@mui/icons-material/ArrowForward'
+import { Box, Typography } from '@mui/material'
+
 import logo from '../../assets/images/atrium-logo-large.png'
 import logotype from '../../assets/images/atrium-logotype-small.png'
 import { AButton, Stepper } from '../../components'
-
 import { palette } from '../../MuiTheme'
 
 export const LoginSubLayout = ({
@@ -20,12 +20,11 @@ export const LoginSubLayout = ({
   goBack?: AnyFunction
   enable?: boolean
 }) => {
-
   const handleBack = () => {
-    if(goBack) goBack()
+    if (goBack) goBack()
   }
   const handleForward = () => {
-    if(goForward && enable) goForward()
+    if (goForward && enable) goForward()
   }
   return (
     <Box
@@ -50,20 +49,35 @@ export const LoginSubLayout = ({
         <Box height="100%">{children}</Box>
       </Box>
       <Box>
-        <Box display={`${stepper ? 'flex' : 'none !important'}`} flexDirection="column" gap="32px">
+        <Box
+          display={`${stepper ? 'flex' : 'none !important'}`}
+          flexDirection="column"
+          gap="32px"
+        >
           <Box>
-            <AButton onClick={handleForward} className="primary active large" color0btn={enable ? palette.secondary.light : palette.text.disabled}>
-              Next &nbsp;<ArrowForwardIcon sx={{fontSize: '28px'}} />
+            <AButton
+              onClick={handleForward}
+              className="primary active large"
+              color0btn={
+                enable ? palette.secondary.light : palette.text.disabled
+              }
+            >
+              Next &nbsp;
+              <ArrowForwardIcon sx={{ fontSize: '28px' }} />
             </AButton>
           </Box>
           <Box>
             <Box onClick={handleBack}>
               <ArrowBackIcon />
-              <Typography variant="body1" color={palette.text.primary} textTransform="capitalize">
+              <Typography
+                variant="body1"
+                color={palette.text.primary}
+                textTransform="capitalize"
+              >
                 &nbsp;go back
               </Typography>
             </Box>
-            </Box>
+          </Box>
           <Box>
             <Stepper length={4} step={2} />
           </Box>
