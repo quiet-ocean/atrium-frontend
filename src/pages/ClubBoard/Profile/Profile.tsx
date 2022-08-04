23d5d260c7c7b0b22641c1ddeb61ef88340261bc
chris bishop <ychrisbishop@gmail.com>
2022-08-04 04:06:59 +0100

    Update community carousel with custom buttons

--- src/pages/ClubBoard/Profile/Profile.tsx
+++ src/pages/ClubBoard/Profile/Profile.tsx
@@ -1,6 +1,7 @@
 import { Box, Typography, Grid } from '@mui/material'
 import { styled } from '@mui/material/styles'
 import { useState } from 'react'
+import Carousel from 'react-material-ui-carousel'
 
 import avatar1 from '../../../assets/images/avatar-8.png'
 import banner from '../../../assets/images/banner-3.png'
@@ -83,64 +84,142 @@ export const UserInfo = () => {
 
 export const CommunityPanel = () => {
   return (
-    <Panel>
-      <Box display="flex" gap="24px" position="relative">
-        <Box width="250px !important" height="250px">
-          <img src={project6} alt="" width="100%" height="100%" />
-        </Box>
-        <Box py="20px" flex="1">
-          <Typography
-            sx={{
-              fontFamily: 'Fractul Alt',
-              fontSize: '36px',
-              fontStyle: 'normal',
-              fontWeight: '600',
-              lineHeight: '120%',
-              textTransform: 'capitalize',
-            }}
+    <Box display="flex" gap="24px" position="relative">
+      <Box width="250px !important" height="250px">
+        <img src={project6} alt="" width="100%" height="100%" />
+      </Box>
+      <Box py="20px" flex="1">
+        <Typography
+          sx={{
+            fontFamily: 'Fractul Alt',
+            fontSize: '36px',
+            fontStyle: 'normal',
+            fontWeight: '600',
+            lineHeight: '120%',
+            textTransform: 'capitalize',
+          }}
+        >
+          atrium
+        </Typography>
+        <Box mt="40px">
+          <AButton
+            className="primary outlined active"
+            btn0color={palette.secondary.light}
           >
-            atrium
-          </Typography>
-          <Box mt="40px">
-            <AButton
-              className="primary outlined active"
-              btn0color={palette.secondary.light}
-            >
-              view community hub
-            </AButton>
-          </Box>
-          <Box mt="20px" display="flex" flexDirection="column" gap="8px">
-            <Box display="flex">
-              <Typography variant="caption" py="5px" flex="2">roles</Typography>  
-              <Box flex="3">
-                <Box>
-                  <AButton className="primary active tag tag-small" btn0color="#FF75CD">whitelisted</AButton>
-                </Box>
+            view community hub
+          </AButton>
+        </Box>
+        <Box mt="20px" display="flex" flexDirection="column" gap="8px">
+          <Box display="flex">
+            <Typography variant="caption" py="5px" flex="2">
+              roles
+            </Typography>
+            <Box flex="3">
+              <Box>
+                <AButton
+                  className="primary active tag tag-small"
+                  btn0color="#FF75CD"
+                >
+                  whitelisted
+                </AButton>
               </Box>
             </Box>
-            <Box display="flex">
-              <Typography variant="caption" py="5px" flex="2">NFT Owned</Typography>  
-              <Typography variant="h4" flex="3">0</Typography>  
-            </Box>
-            <Box display="flex">
-              <Typography variant="caption" py="5px" flex="2">joined</Typography>  
-              <Typography variant="h4" flex="3">august, 2nd 2022</Typography>  
-            </Box>
+          </Box>
+          <Box display="flex">
+            <Typography variant="caption" py="5px" flex="2">
+              NFT Owned
+            </Typography>
+            <Typography variant="h4" flex="3">
+              0
+            </Typography>
+          </Box>
+          <Box display="flex">
+            <Typography variant="caption" py="5px" flex="2">
+              joined
+            </Typography>
+            <Typography variant="h4" flex="3">
+              august, 2nd 2022
+            </Typography>
           </Box>
         </Box>
-        <Box sx={{ position: 'absolute', top: '0px', right: '0px' }}>
-          <AButton className="outlined primary" btn0color={palette.secondary.light}>view all</AButton>
-        </Box>
       </Box>
-    </Panel>
+      <Box sx={{ position: 'absolute', right: '0px', top: '0px' }}>
+        <AButton
+          className="outlined primary"
+          btn0color={palette.secondary.light}
+        >
+          view all
+        </AButton>
+      </Box>
+    </Box>
   )
 }
 export const CommunityCarousel = () => {
   return (
-    <Box display="flex" flexDirection="column" justifyContent="space-between">
-      <CommunityPanel />
-      <Box flex="1"></Box>
-    </Box>
+    <Panel>
+      <Carousel
+        navButtonsAlwaysVisible
+        autoPlay={false}
+        indicators={true}
+        NextIcon={
+          <AButton className="primary active" btn0color={palette.text.disabled}>
+            Next
+          </AButton>
+        }
+        PrevIcon={
+          <AButton className="primary active" btn0color={palette.text.disabled}>
+            Prev
+          </AButton>
+        }
+        navButtonsProps={{
+          // Change the colors and radius of the actual buttons. THIS STYLES BOTH BUTTONS
+          style: {
+            backgroundColor: 'cornflowerblue',
+            borderRadius: 0,
+            bottom: 0,
+            left: 0,
+            padding: 0,
+            // position: 'absolute',
+          },
+        }}
+        navButtonsWrapperProps={{
+          // Move the buttons to the bottom. Unsetting top here to override default style.
+          style: {
+            bottom: '0',
+            top: 'unset',
+          },
+        }}
+        IndicatorIcon={<Box sx={{}}></Box>}
+        indicatorIconButtonProps={{
+          style: {
+            backgroundColor: palette.text.disabled,
+            borderRadius: '0px',
+            // 1
+            color: 'blue',
+
+            flex: 1,
+
+            height: '4px',
+            width: 'auto', // 3
+          },
+        }}
+        activeIndicatorIconButtonProps={{
+          style: {
+            backgroundColor: 'red', // 2
+          },
+        }}
+        indicatorContainerProps={{
+          style: {
+            display: 'flex',
+            gap: 24,
+          },
+        }}
+      >
+        <CommunityPanel />
+        <CommunityPanel />
+        <CommunityPanel />
+      </Carousel>
+    </Panel>
   )
 }
 export const Tags = () => {
@@ -167,7 +246,9 @@ export const Tags = () => {
       </Box>
       <Box pt="24px">
         <Typography variant="body1">
-          I have been coding for 8 years. I’ve built 16 websites using React and Javascript mainly. I’ve built games as well, 3 in Unity and 2 using custom APIs.
+          I have been coding for 8 years. I’ve built 16 websites using React and
+          Javascript mainly. I’ve built games as well, 3 in Unity and 2 using
+          custom APIs.
         </Typography>
       </Box>
     </Panel>
