29140dbcba0086154d7ab443435d23afab77d363
chris bishop <ychrisbishop@gmail.com>
2022-08-05 21:00:23 +0200

    Update EditWallet.tsx

--- src/pages/ClubBoard/EditProfile/EditWallet.tsx
+++ src/pages/ClubBoard/EditProfile/EditWallet.tsx
@@ -3,8 +3,17 @@ import React from 'react'
 import { AButton } from '../../../components'
 import { palette } from '../../../MuiTheme'
 import { AntSwitch } from './styled'
+import CheckCircleIcon from '@mui/icons-material/CheckCircle';
 
 export const EditWallet = () => {
+  const InfoItem = ({ index, children }: { index: string, children: React.ReactNode }) => {
+    return (
+      <Box display="flex" py="8px">
+        <Typography variant="body2" width="175px" sx={{ textTransform: 'capitalize' }}>{ index }</Typography>
+        {children}
+      </Box>
+    )
+  }
   return (
     <Box>
       <Grid container>
@@ -15,7 +24,23 @@ export const EditWallet = () => {
               Details on your wallet source and connection.
             </Typography>
             <Box p="24px 80px">
-              
+              <InfoItem index='status'>
+                <Box display="flex" gap="8px">
+                 <CheckCircleIcon sx={{color: '#90E487'}} />
+                 <Typography variant="h5" py="2px" color="#90E487">
+                   connected
+                 </Typography>
+               </Box>
+              </InfoItem>
+              <InfoItem index="wallet id">
+                <Typography variant="h5">swiftyyy.near</Typography>
+              </InfoItem>
+              <InfoItem index="wallet source">
+                <Typography variant="h5" sx={{ textTransform: 'uppercase' }}>near</Typography>
+              </InfoItem>
+              <Box mt="32px">
+                <Typography variant="h6" color={palette.text.disabled} sx={{ textDecoration: 'underline'}}>Disconnect Wallet</Typography>
+              </Box>
             </Box>
           </Box>
         </Grid>
@@ -29,7 +54,7 @@ If public, anyone on Atrium can message you.
             <Box p="24px 0px">
               <Box display="flex" gap="12px">
                 <AntSwitch />
-                <Typography variant="caption" py="2px">Displayed on Profile</Typography>
+                <Typography variant="caption" py="2px">Private</Typography>
               </Box>
             </Box>
           </Box>
