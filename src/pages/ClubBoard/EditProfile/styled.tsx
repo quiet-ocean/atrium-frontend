74edd40766933c1cca15e9ceafe92a92bf5b29eb
chris bishop <ychrisbishop@gmail.com>
2022-08-05 20:12:30 +0200

    Create Edit Identity Page

--- src/pages/ClubBoard/EditProfile/styled.tsx
+++ src/pages/ClubBoard/EditProfile/styled.tsx
@@ -77,42 +77,42 @@ export const AntSwitch = styled(Switch)(({ theme }) => ({
   '& .MuiSwitch-switchBase': {
     '&.Mui-checked': {
       '& + .MuiSwitch-track': {
-        backgroundColor: theme.palette.mode === 'dark' ? '#177ddc' : '#1890ff',
+        backgroundColor: theme.palette.mode === 'dark' ? '#90E487' : '#1890ff',
         opacity: 1,
       },
       color: '#fff',
-      transform: 'translateX(30px)',
+      transform: 'translateX(18px)',
     },
     padding: 2,
   },
   '& .MuiSwitch-thumb': {
     borderRadius: 10,
     boxShadow: '0 2px 4px 0 rgb(0 35 11 / 20%)',
-    height: 20,
+    height: 16,
     transition: theme.transitions.create(['width'], {
       duration: 200,
     }),
-    width: 20,
+    width: 16,
   },
   '& .MuiSwitch-track': {
     backgroundColor:
       theme.palette.mode === 'dark'
         ? 'rgba(255,255,255,.35)'
         : 'rgba(0,0,0,.25)',
-    borderRadius: 24 / 2,
+    borderRadius: 20 / 2,
     boxSizing: 'border-box',
     opacity: 1,
   },
   '&:active': {
     '& .MuiSwitch-switchBase.Mui-checked': {
-      transform: 'translateX(30px)',
+      transform: 'translateX(18px)',
     },
     '& .MuiSwitch-thumb': {
-      width: 20,
+      width: 16,
     },
   },
   display: 'flex',
-  height: 24,
+  height: 20,
   padding: 0,
-  width: 56,
+  width: 40,
 }))
