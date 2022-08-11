4a42abdb8c7afb14cbf59400feda6b15c0707d1e
chris bishop <ychrisbishop@gmail.com>
2022-08-11 05:28:59 +0200

    Update login success page

--- src/pages/LoginSuccess.tsx
+++ src/pages/LoginSuccess.tsx
@@ -6,6 +6,10 @@ import { LoginLayout, Stepper } from '../components'
 import { useAppSelector } from '../hooks'
 import phaserGame from '../PhaserGame'
 import type Bootstrap from '../scenes/Bootstrap'
+import { LoginSubLayout } from './Login/LoginSubLayout'
+
+import { palette } from '../MuiTheme'
+import avatar from '../assets/images/avatar-9.png'
 
 const LoginSuccess = () => {
   const user = useAppSelector((state) => state.auth.user)
@@ -14,44 +18,52 @@ const LoginSuccess = () => {
 
   const navigate = useNavigate()
 
-  const handleNextBtn = () => {
-    if (!roomJoined && lobbyJoined) {
-      const bootstrap = phaserGame.bootstrap as Bootstrap
-      bootstrap.network
-        .joinOrCreatePublic()
-        .then(() => {
-          bootstrap.launchGame()
-          navigate('/game')
-        })
-        .catch((error) => console.error(error))
-    }
-  }
+  // const handleNextBtn = () => {
+  //   if (!roomJoined && lobbyJoined) {
+  //     const bootstrap = phaserGame.bootstrap as Bootstrap
+  //     bootstrap.network
+  //       .joinOrCreatePublic()
+  //       .then(() => {
+  //         bootstrap.launchGame()
+  //         navigate('/game')
+  //       })
+  //       .catch((error) => console.error(error))
+  //   }
+  // }
 
   return (
-    <LoginLayout>
-      <Box sx={{ mb: '50px', textAlign: 'center' }}>
-        <img src={(user as any).avatar} alt="" style={{ width: 230 }} />
-      </Box>
-      <Box sx={{ mb: '50px' }}>
-        <Typography className="atrium_title" sx={{ mb: '50px' }} component="h1">
-          Welcome Back!
-        </Typography>
-        <Typography className="atrium_info">Welcome Back to Atrium,</Typography>
-        <Typography className="atrium_info">
-          {(user as any).username}
-        </Typography>
-      </Box>
-      <Box sx={{ mb: '24px' }}>
-        <Button
-          className="atrium_btn atrium_btn_primary"
-          onClick={handleNextBtn}
-        >
-          NEXT
-        </Button>
-      </Box>
-      <Box>
-        <Stepper length={5} step={5} />
-      </Box>
+    <LoginLayout> 
+      <LoginSubLayout stepper>
+        <Box flexDirection="column" gap={`32px`}>
+          <Box mt="32px" justifyContent="center">
+            <Box
+              width="160px"
+              height="160px"
+              borderRadius="102px"
+              border={`1px solid ${palette.text.primary}`}
+            >
+              {avatar && (
+                <img
+                  src={avatar}
+                  width="160px"
+                  height="160px"
+                  style={{
+                    border: `1px solid ${palette.text.primary}`,
+                    borderRadius: `50%`,
+                  }}
+                />
+              )}
+            </Box>
+          </Box>
+          <Box sx={{ mb: '50px' }} flexDirection="column" gap="12px">
+            <Typography variant="h3" sx={{ textAlign: 'center' }}>Welcome Back!</Typography>
+            <Typography variant="h3" sx={{ textAlign: 'center' }}>Welcome Back to Atrium,</Typography>
+            <Typography variant="h4" sx={{ textAlign: 'center' }}>
+              {(user as any).username}
+            </Typography>
+          </Box>
+        </Box>
+      </LoginSubLayout>
     </LoginLayout>
   )
 }
