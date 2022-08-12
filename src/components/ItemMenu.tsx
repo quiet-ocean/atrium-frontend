5dbe04d2cee600a36b69aa20fc35937f74dbb58e
chris bishop <ychrisbishop@gmail.com>
2022-08-12 18:36:44 +0200

    Update ItemMenu.tsx

--- src/components/ItemMenu.tsx
+++ src/components/ItemMenu.tsx
@@ -14,6 +14,29 @@ const Divider = styled(Box)(() => ({
   width: '0px',
 }))
 
+const AnimationWrapper = styled(Box)`
+  @keyframes collapse-open {
+    0% {
+      height: 0px;
+    }
+    100% {
+      height: 80x;
+    }
+  }
+
+  &.open {
+    animation: collapse-open;
+    animation-duration: 0.5s;
+    height: 80px;
+  }
+  &.close {
+    height: 0px;
+    & * {
+      height: 0px;
+      padding: 0px;
+    }
+  }
+`
 const Item = ({ img }: { img?: string }) => {
   return (
     <Box width="48px" height="48px" sx={{ background: '#A8A8A8' }}>
@@ -36,9 +59,14 @@ export const ItemMenu = () => {
     >
       <Box
         sx={{
-          backdropFilter: 'opacity(20%)',
-          backgroundColor: 'rgba(26, 26, 26, 0.9)',
+          display: 'flex',
+
+          flexDirection: 'column',
+          // backdropFilter: 'opacity(20%)',
+          // backgroundColor: 'rgba(26, 26, 26, 0.9)',
           // width:'100%',
+          height: '116px',
+          justifyContent: 'end',
         }}
       >
         <Box
@@ -47,6 +75,10 @@ export const ItemMenu = () => {
           p="6px"
           // width='100%'
           onClick={() => setOpen(!open)}
+          sx={{
+            backdropFilter: 'opacity(20%)',
+            backgroundColor: 'rgba(26, 26, 26, 0.9)',
+          }}
         >
           {open ? (
             <KeyboardArrowDownIcon sx={{ color: 'white' }} />
@@ -54,20 +86,30 @@ export const ItemMenu = () => {
             <KeyboardArrowUpIcon sx={{ color: 'white' }} />
           )}
         </Box>
-        <Box
-          p="16px"
-          sx={{ borderTop: `1px solid white`, display: open ? 'flex' : 'none' }}
-        >
-          <Box>
-            <Item img={item1} />
-          </Box>
-          {new Array(5).fill(2).map((_, key: number) => (
-            <Box key={key} display="flex">
-              <Divider />
-              <Item />
+        <AnimationWrapper className={open ? 'open' : 'close'}>
+          <Box
+            p="16px"
+            sx={{
+              backdropFilter: 'opacity(20%)',
+
+              backgroundColor: 'rgba(26, 26, 26, 0.9)',
+
+              borderTop: `1px solid white`,
+              // display: open ? 'flex' : 'none',
+              display: 'flex',
+            }}
+          >
+            <Box>
+              <Item img={item1} />
             </Box>
-          ))}
-        </Box>
+            {new Array(5).fill(2).map((_, key: number) => (
+              <Box key={key} display="flex">
+                <Divider />
+                <Item />
+              </Box>
+            ))}
+          </Box>
+        </AnimationWrapper>
       </Box>
     </Box>
   )
