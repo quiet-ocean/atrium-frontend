79b2b66dbd5a31c407af1882da4add988f1b3543
chris bishop <ychrisbishop@gmail.com>
2022-08-17 17:22:21 +0200

    Update index.ts

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,7 +1,5 @@
-export * from './Setting'
-export * from './ConnectSocials'
 export * from './GameUI'
-export * from './ScanDAO'
-export * from './LoginSuccess'
-
 export * from './Login'
+export * from './ClubBoard'
+export * from './ClubBoardUI'
+
