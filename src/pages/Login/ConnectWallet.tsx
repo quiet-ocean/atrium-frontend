2f06d4a26cef81be0ed24d362f6cebada211d99b
chris bishop <ychrisbishop@gmail.com>
2022-08-17 19:12:32 +0200

    Update ConnectWallet.tsx

--- src/pages/Login/ConnectWallet.tsx
+++ src/pages/Login/ConnectWallet.tsx
@@ -127,7 +127,7 @@ const ConnectWallet = () => {
         stepper
         step={1}
         enable={enable}
-        goForward={() => fetchLogin(`/success`, `/set-name`)}
+        goForward={() => fetchLogin(`/set-name`, `/set-name`)}
         goBack={() => navigate('/signin')}
       >
         <Box flexDirection="column">
