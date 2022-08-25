badcd77d769e73f98fe597e4df9425f3dc20fd6b
chris bishop <ychrisbishop@gmail.com>
2022-08-25 17:03:17 +0200

    Create global snack system

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import { Container } from '@mui/material'
+import { Container, Snackbar, Alert } from '@mui/material'
 import { useEffect } from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 // import { PrivateRoute } from './components'
-import { useAppDispatch } from './hooks'
+import { useAppDispatch, useAppSelector } from './hooks'
 import {
   ConnectWallet,
   SelectIdentity,
@@ -16,17 +16,26 @@ import {
   ClubBoardUI,
   LoginSuccess,
 } from './pages'
+import { setSnackbarOpen } from './stores/AppStore'
 import { setWalletConnected } from './stores/UserStore'
 import { getAccount } from './utils'
 
 const App = () => {
+  const vertical = 'top'
+  const horizontal = 'right'
+
   const dispatch = useAppDispatch()
+
+  const open = useAppSelector((state) => state.app.snack.open)
+  const message = useAppSelector((state) => state.app.snack.content)
+  const type = useAppSelector((state) => state.app.snack.type)
 
   useEffect(() => {
     const account = getAccount()
     dispatch(setWalletConnected(account.accountId !== ''))
   }, [])
 
+  const handleClose = () => dispatch(setSnackbarOpen(false))
   return (
     <Container
       maxWidth="xl"
@@ -47,6 +56,26 @@ const App = () => {
           <Route path="/success" element={<LoginSuccess />} />
         </Routes>
       </BrowserRouter>
+      <Snackbar
+        anchorOrigin={{ horizontal, vertical }}
+        open={open}
+        onClose={handleClose}
+        message={message}
+        key={vertical + horizontal}
+        autoHideDuration={5000}
+      >
+        <Alert
+          onClose={handleClose}
+          severity={type}
+          sx={{
+            borderRadius: 0,
+            fontFamily: 'Fractul Alt',
+            width: '100%',
+          }}
+        >
+          {message}
+        </Alert>
+      </Snackbar>
     </Container>
   )
 }
