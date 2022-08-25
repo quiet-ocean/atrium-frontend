af4e5a88434f7be799a69071b7ba06eb0d6553cc
chris bishop <ychrisbishop@gmail.com>
2022-08-25 18:18:29 +0200

    Integrate community to api

--- src/pages/ClubBoard/Profile/CommunityCarousel.tsx
+++ src/pages/ClubBoard/Profile/CommunityCarousel.tsx
@@ -2,70 +2,77 @@ import { Box } from '@mui/material'
 import Carousel from 'react-material-ui-carousel'
 
 import { palette } from '../../../MuiTheme'
+import type { ICommunityMember } from '../../../types/model'
+import { ICommunity } from '../../../types/model'
 import { Community as Panel } from '../styled'
 
 import { CommunityPanel } from './CommunityPanel'
 
-export const CommunityCarousel = () => {
+const settings = {
+  IndicatorIcon: <Box sx={{}}></Box>,
+  NextIcon: <>Next</>,
+  PrevIcon: <>Prev</>,
+  activeIndicatorIconButtonProps: {
+    style: {
+      backgroundColor: 'red', // 2
+    },
+  },
+  autoPlay: false,
+  indicatorContainerProps: {
+    style: {
+      display: 'flex',
+      gap: 24,
+    },
+  },
+  indicatorIconButtonProps: {
+    style: {
+      backgroundColor: palette.text.disabled,
+      borderRadius: '0px',
+      // 1
+      color: 'blue',
+
+      flex: 1,
+
+      height: '4px',
+      width: 'auto', // 3
+    },
+  },
+  indicators: true,
+  navButtonsAlwaysVisible: true,
+  navButtonsProps: {
+    // Change the colors and radius of the actual buttons. THIS STYLES BOTH BUTTONS
+    style: {
+      background: palette.background.paper,
+      borderRadius: 0,
+      bottom: 0,
+      color: palette.text.primary,
+      fontFamily: 'Fractul',
+      fontSize: '16px',
+      fontWeight: 600,
+      left: 0,
+      padding: '4px 8px',
+      // position: 'absolute',
+    },
+  },
+  navButtonsWrapperProps: {
+    // Move the buttons to the bottom. Unsetting top here to override default style.
+    style: {
+      bottom: '0',
+      top: 'unset',
+    },
+  },
+}
+export const CommunityCarousel = ({
+  members,
+}: {
+  members: string[] | ICommunityMember[]
+}) => {
   return (
     <Panel>
-      <Carousel
-        navButtonsAlwaysVisible
-        autoPlay={false}
-        indicators={true}
-        NextIcon={<>Next</>}
-        PrevIcon={<>Prev</>}
-        navButtonsProps={{
-          // Change the colors and radius of the actual buttons. THIS STYLES BOTH BUTTONS
-          style: {
-            background: palette.background.paper,
-            borderRadius: 0,
-            bottom: 0,
-            color: palette.text.primary,
-            fontFamily: 'Fractul',
-            fontSize: '16px',
-            fontWeight: 600,
-            left: 0,
-            padding: '4px 8px',
-            // position: 'absolute',
-          },
-        }}
-        navButtonsWrapperProps={{
-          // Move the buttons to the bottom. Unsetting top here to override default style.
-          style: {
-            bottom: '0',
-            top: 'unset',
-          },
-        }}
-        IndicatorIcon={<Box sx={{}}></Box>}
-        indicatorIconButtonProps={{
-          style: {
-            backgroundColor: palette.text.disabled,
-            borderRadius: '0px',
-            // 1
-            color: 'blue',
-
-            flex: 1,
-
-            height: '4px',
-            width: 'auto', // 3
-          },
-        }}
-        activeIndicatorIconButtonProps={{
-          style: {
-            backgroundColor: 'red', // 2
-          },
-        }}
-        indicatorContainerProps={{
-          style: {
-            display: 'flex',
-            gap: 24,
-          },
-        }}
-      >
-        <CommunityPanel />
-        <CommunityPanel />
-        <CommunityPanel />
+      <Carousel {...settings}>
+        {members.map((item: any, key: number) => (
+          <CommunityPanel member={item} key={key} />
+        ))}
       </Carousel>
       {/* <Box display="flex" gap="24px">
         <Box display="flex" gap="24px"></Box>
--- src/pages/ClubBoard/Profile/CommunityPanel.tsx
+++ src/pages/ClubBoard/Profile/CommunityPanel.tsx
@@ -3,19 +3,27 @@ import { Box, Typography } from '@mui/material'
 import project6 from '../../../assets/images/project-6.png'
 import { AButton } from '../../../components'
 import { palette } from '../../../MuiTheme'
+import type { ICommunity, ICommunityMember } from '../../../types/model'
+import { convertStr2LongDate } from '../../../utils'
 
-export const CommunityPanel = () => {
+export const CommunityPanel = ({ member }: { member: ICommunityMember }) => {
+  const community: ICommunity = member.community as ICommunity
+
+  const handleCommunityHub = (id: string) => {
+    console.log('go to community hub for community ', id)
+  }
   return (
     <Box display="flex" gap="24px" position="relative">
       <Box width="250px !important" height="250px">
         <img src={project6} alt="" width="100%" height="100%" />
       </Box>
-      <Box py="20px" flex="1">
-        <Typography variant="h2">atrium</Typography>
+      <Box py="24px" flex="1">
+        <Typography variant="h2">{community.name}</Typography>
         <Box mt="40px">
           <AButton
             className="primary outlined active"
             color0btn={palette.secondary.light}
+            onClick={() => handleCommunityHub(community._id)}
           >
             view community hub
           </AButton>
@@ -49,7 +57,8 @@ export const CommunityPanel = () => {
               joined
             </Typography>
             <Typography variant="h4" flex="3">
-              august, 2nd 2022
+              {/* august, 2nd 2022 */}
+              {convertStr2LongDate(member.createdAt)}
             </Typography>
           </Box>
         </Box>
