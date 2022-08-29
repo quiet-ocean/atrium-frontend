b3c82af6311cea254b48e3d96f52b3eac3335d01
chris bishop <ychrisbishop@gmail.com>
2022-08-29 06:00:24 +0200

    Update post/comment

--- src/pages/ClubBoard/Post/Comments.tsx
+++ src/pages/ClubBoard/Post/Comments.tsx
@@ -1,41 +1,86 @@
 import SendIcon from '@mui/icons-material/Send'
 import SentimentSatisfiedAltIcon from '@mui/icons-material/SentimentSatisfiedAlt'
 import { Box, Typography } from '@mui/material'
+import { useState } from 'react'
+import ScrollableFeed from 'react-scrollable-feed'
 
 import { AtButton, AdornmentInput, EmptyBox } from '../../../components'
+import { useAppDispatch } from '../../../hooks'
 import { palette } from '../../../MuiTheme'
+import type { TSnack } from '../../../stores/AppStore'
+import { openSnack } from '../../../stores/AppStore'
 import type { IComment } from '../../../types/model'
 // import avatar from '../images/avatar-3.png'
 
 export const Comment = ({ data }: { data: IComment }) => {
   return (
-    <Box display="flex" gap="8px" p="12px 0px">
+    <Box display="flex" gap="8px" p="12px 4px">
       <Box py="10px">
-        <img src={data.author?.avatar} alt="" width="45px" height="45px" />
+        <img
+          src={data.author?.avatar}
+          alt=""
+          width="45px"
+          height="45px"
+          style={{ borderRadius: 45 }}
+        />
       </Box>
-      <Box p="12px" sx={{ background: `${palette.background.paper}` }}>
+      <Box
+        p="12px"
+        sx={{ background: `${palette.background.paper}`, width: '100%' }}
+      >
         <Typography variant="caption">{data.body}</Typography>
       </Box>
     </Box>
   )
 }
-export const Comments = ({ data }: { data: IComment[] }) => {
+export const Comments = ({
+  data,
+  createComment,
+}: {
+  data: IComment[]
+  createComment: (body: string) => void
+}) => {
+  const dispatch = useAppDispatch()
+  const [value, setValue] = useState('')
+
+  const handleCreate = () => {
+    if (value) createComment(value)
+    else {
+      const snack: TSnack = {
+        content: 'Please fill in the comment',
+        open: true,
+        type: 'warning',
+      }
+      dispatch(openSnack(snack))
+    }
+  }
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setValue(e.target.value)
+  }
   return (
     <Box p="30px" border={`1px solid ${palette.background.paper}`} mt="48px">
       <Box display="flex" justifyContent="space-between">
         <Typography variant="h4">comments</Typography>
         <AtButton variant="small" text="see all" />
       </Box>
-      <Box mt="12px">
-        {data && data.length > 0 ? (
-          <>
-            {data.map((item: IComment, key: number) => (
-              <Comment data={item} key={key} />
-            ))}
-          </>
-        ) : (
-          <EmptyBox>No Commnet Yet</EmptyBox>
-        )}
+      <Box
+        mt="12px"
+        sx={{
+          maxHeight: 200,
+          // overflowY: 'scroll',
+        }}
+      >
+        <ScrollableFeed>
+          {data && data.length > 0 ? (
+            <>
+              {data.map((item: IComment, key: number) => (
+                <Comment data={item} key={key} />
+              ))}
+            </>
+          ) : (
+            <EmptyBox>No Commnet Yet</EmptyBox>
+          )}
+        </ScrollableFeed>
       </Box>
       <Box display="flex" gap="12px">
         <Box
@@ -49,7 +94,9 @@ export const Comments = ({ data }: { data: IComment[] }) => {
         <AdornmentInput
           variant="default"
           label="type here..."
-          adornment={<SendIcon />}
+          value={value}
+          onChange={handleChange}
+          adornment={<SendIcon onClick={handleCreate} />}
         />
       </Box>
     </Box>
--- src/pages/ClubBoard/Post/PostItem.tsx
+++ src/pages/ClubBoard/Post/PostItem.tsx
@@ -1,23 +1,15 @@
 import { Box, Typography } from '@mui/material'
-import { styled } from '@mui/material/styles'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
 // import postImage from '../../../assets/images/post-image.png'
 import { AtButton } from '../../../components'
+import { useAppSelector } from '../../../hooks'
 import { palette } from '../../../MuiTheme'
 import type { IPost } from '../../../types/model'
+import { apiGetRequest, apiPostRequest } from '../../../utils'
 import { convertString2LongDate } from '../../../utils/util'
 
 import { Comments } from './Comments'
-
-const PostText = styled(Typography)(({ theme }) => ({
-  color: theme.palette.text.disabled,
-  fontFamily: 'Andale Mono Regular',
-  fontSize: '24px !important',
-  fontWeight: '400 !important',
-  letterSpacing: '-0.05em',
-  lineHeight: '27px',
-}))
 
 export const Tag = ({ children }: { children: React.ReactNode }) => {
   return (
@@ -28,9 +20,47 @@ export const Tag = ({ children }: { children: React.ReactNode }) => {
   )
 }
 
-export const PostItem = ({ data }: { data: IPost }) => {
-  const [expand, setExpand] = useState(false)
+const url = process.env.VITE_API_URL || 'localhost:2567'
 
+export const PostItem = ({ data }: { data: IPost }) => {
+  // console.log('Post data: ', data)
+  const [expand, setExpand] = useState(false)
+  const [post, setPost] = useState<IPost>(data)
+
+  const me = useAppSelector((state) => state.auth.user)
+
+  const getComments = async () => {
+    const res = await apiGetRequest(`${url}/posts/${post._id}/comment`)
+    // console.log('Got comments: ', res.data)
+    if (res.status === 200 && res.data && res.data.comments) {
+      setPost({ ...post, comments: res.data.comments })
+    } else {
+      console.log('Something went wrong while load comments for post')
+    }
+  }
+  useEffect(() => {
+    if (!post && post === {}) return
+
+    if (post.comments.length > 0 && typeof post.comments[0] === 'string') {
+      getComments()
+    }
+  }, [post])
+  const createComment = async (body: string) => {
+    const id = data._id
+    const res = await apiPostRequest(`${url}/posts/${id}/comment`, {
+      body,
+    })
+
+    if (res.status === 200 && res.data) {
+      console.log('New comment: ', res.data)
+      setPost({
+        ...post,
+        comments: [...post.comments, { ...res.data, author: me }],
+      })
+    } else {
+      console.log('Something went wrong while create comment')
+    }
+  }
   return (
     <Box sx={{ border: `1px solid ${palette.text.primary}`, padding: '36px' }}>
       <Box
@@ -39,16 +69,14 @@ export const PostItem = ({ data }: { data: IPost }) => {
         onClick={() => setExpand(!expand)}
       >
         <Typography variant="h2" sx={{ fontSize: '60px', maxWidth: '640px' }}>
-          {/* spotify integrating with atrium */}
-          {data.title}
+          {post.title}
         </Typography>
         <Box>
           <Typography
             variant="body2"
             sx={{ fontSize: '20px', textTransform: 'uppercase' }}
           >
-            {/* //mar 1st, 2022 */}
-            {convertString2LongDate(data.createdAt)}
+            {convertString2LongDate(post.createdAt)}
           </Typography>
         </Box>
       </Box>
@@ -59,47 +87,16 @@ export const PostItem = ({ data }: { data: IPost }) => {
           <Tag>trending news</Tag>
           <Tag>integrations</Tag>
         </Box>
-        {/* <Box mt="48px">
-          <PostText>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Amet quam
-            in purus maecenas nisl tincidunt. Nascetur justo adipiscing lectus
-            sapien sit accumsan. Platea ultrices est odio neque. Quam hendrerit
-            amet, tellus lobortis lacus. Arcu amet, eu, dignissim gravida. A
-            turpis ut id amet sollicitudin leo fusce integer.
-          </PostText>
-        </Box>
-        <Typography variant="h2" sx={{ textAlign: 'center', py: '24px' }}>
-          “spotify integrating with atrium”
-        </Typography>
-        <PostText>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Turpis eu sed
-          et tortor proin. Ac vulputate eget sagittis amet metus feugiat vitae.
-          Velit nunc, augue felis interdum integer aliquet commodo vel ultrices.
-          Feugiat malesuada tempor euismod et nibh ac laoreet urna, cursus.
-          Feugiat nibh non amet, nunc risus faucibus viverra hendrerit. Cursus
-          sed est tellus lorem nec vel. Lacinia ut rhoncus massa id turpis
-          quisque amet, non.
-        </PostText> */}
         <Box p="24px 0px">
-          {/* <img src={postImage} alt="" /> */}
           <img
-            src={`${process.env.VITE_API_URL}/files/${data.media.path}`}
+            src={`${process.env.VITE_API_URL}/files/${post.media.path}`}
             alt=""
             width="100%"
           />
         </Box>
-        <PostText>
-          {data.body}
-          {/* Lorem ipsum dolor sit amet, consectetur adipiscing elit. Turpis eu sed
-          et tortor proin. Ac vulputate eget sagittis amet metus feugiat vitae.
-          Velit nunc, augue felis interdum integer aliquet commodo vel ultrices.
-          Feugiat malesuada tempor euismod et nibh ac laoreet urna, cursus.
-          Feugiat nibh non amet, nunc risus faucibus viverra hendrerit. Cursus
-          sed est tellus lorem nec vel. Lacinia ut rhoncus massa id turpis
-          quisque amet, non. */}
-        </PostText>
+        <Typography variant="body1">{post.body}</Typography>
       </Box>
-      <Comments data={data.comments} />
+      <Comments data={post.comments} createComment={createComment} />
     </Box>
   )
 }
