eaef4c7b1b8c1b5d7c995088bbda57657544b24a
chris bishop <ychrisbishop@gmail.com>
2022-08-31 05:59:55 +0200

    Update CommunityCarousel - carousel dots and button

--- src/pages/ClubBoard/Profile/CommunityCarousel.tsx
+++ src/pages/ClubBoard/Profile/CommunityCarousel.tsx
@@ -1,96 +1,135 @@
 import { Box, Typography } from '@mui/material'
-import Carousel from 'react-material-ui-carousel'
+import { useState, useEffect, useRef } from 'react'
+import Slider from 'react-slick'
 
+import { Button } from '../../../components'
 import { palette } from '../../../MuiTheme'
 import type { ICommunityMember } from '../../../types/model'
 import { Community as Panel } from '../styled'
 
 import { CommunityPanel } from './CommunityPanel'
 
-const settings = {
-  IndicatorIcon: <Box sx={{}}></Box>,
-  NextIcon: <>Next</>,
-  PrevIcon: <>Prev</>,
-  activeIndicatorIconButtonProps: {
-    style: {
-      backgroundColor: 'red', // 2
-    },
-  },
-  autoPlay: false,
-  indicatorContainerProps: {
-    style: {
-      display: 'flex',
-      gap: 24,
-    },
-  },
-  indicatorIconButtonProps: {
-    style: {
-      backgroundColor: palette.text.disabled,
-      borderRadius: '0px',
-      // 1
-      color: 'blue',
+import 'slick-carousel/slick/slick.css'
+import 'slick-carousel/slick/slick-theme.css'
 
-      flex: 1,
-
-      height: '4px',
-      width: 'auto', // 3
-    },
-  },
-  indicators: true,
-  navButtonsAlwaysVisible: true,
-  navButtonsProps: {
-    // Change the colors and radius of the actual buttons. THIS STYLES BOTH BUTTONS
-    style: {
-      background: palette.background.paper,
-      borderRadius: 0,
-      bottom: 0,
-      color: palette.text.primary,
-      fontFamily: 'Fractul',
-      fontSize: '16px',
-      fontWeight: 600,
-      left: 0,
-      padding: '4px 8px',
-      // position: 'absolute',
-    },
-  },
-  navButtonsWrapperProps: {
-    // Move the buttons to the bottom. Unsetting top here to override default style.
-    style: {
-      bottom: '0',
-      top: 'unset',
-    },
-  },
+const sliderSettings = {
+  arrows: false,
+  centerMode: true,
+  centerPadding: '0px',
+  dots: false,
+  focusOnSelect: true,
+  infinite: true,
+  slidesToScroll: 1,
+  slidesToShow: 1,
+}
+const CarouselDotItem = ({ active }: { active?: boolean }) => {
+  return (
+    <Box
+      sx={{
+        background: active ? palette.secondary.main : palette.text.disabled,
+        height: 4,
+        width: '100%',
+      }}
+    />
+  )
+}
+const CarouselDots = ({
+  length,
+  current,
+}: {
+  length: number
+  current: number
+}) => {
+  return (
+    <Box width="100%" display="flex" gap={3}>
+      {new Array(length).fill(2).map((_, key: number) => (
+        <CarouselDotItem key={key} active={key === current} />
+      ))}
+    </Box>
+  )
 }
 export const CommunityCarousel = ({
   members,
 }: {
   members: ICommunityMember[]
 }) => {
+  const [index, setIndex] = useState(2)
+  const sliderRef = useRef<Slider>(null)
+
+  const handleNext = () => {
+    if (sliderRef && sliderRef.current) {
+      sliderRef.current.slickNext()
+    }
+  }
+
+  const handlePrev = () => {
+    if (sliderRef && sliderRef.current) {
+      sliderRef.current.slickPrev()
+    }
+  }
   return (
-    <Panel>
+    <Panel
+      sx={{
+        '& .slick-current': {
+          background: 'none',
+          borderRadius: 0,
+          padding: 0,
+        },
+      }}
+    >
       {members && members.length ? (
-        <Carousel {...settings}>
+        <Slider
+          ref={sliderRef}
+          {...sliderSettings}
+          beforeChange={(_, idx: number) => {
+            console.log('current carousel index: ', idx)
+            setIndex(idx)
+          }}
+        >
           {members.map((item: any, key: number) => (
             <CommunityPanel member={item} key={key} />
           ))}
-        </Carousel>
+          {/* {members.map((item: any, key: number) => (
+            <CommunityPanel member={item} key={key} />
+          ))}
+          {members.map((item: any, key: number) => (
+            <CommunityPanel member={item} key={key} />
+          ))}
+          {members.map((item: any, key: number) => (
+            <CommunityPanel member={item} key={key} />
+          ))} */}
+        </Slider>
       ) : (
         <Box p="24px" display="flex" justifyContent="center">
           <Typography variant="h5">No Joined Community</Typography>
         </Box>
       )}
 
-      {/* <Box display="flex" gap="24px">
-        <Box display="flex" gap="24px"></Box>
+      <Box display="flex" gap="24px">
+        <Box display="flex" py={2} gap="24px" width="100%">
+          <CarouselDots
+            length={members && members.length ? members.length : 1}
+            current={index}
+          />
+          {/* <CarouselDots length={4} current={index} /> */}
+        </Box>
         <Box display="flex" gap="16px">
-          <Button className="primary active" color0btn={palette.text.disabled}>
+          <Button
+            onClick={handlePrev}
+            className="primary active"
+            color0btn={palette.text.disabled}
+          >
             Prev
           </Button>
-          <Button className="primary active" color0btn={palette.text.disabled} ref={nextButtonRef}>
+          <Button
+            onClick={handleNext}
+            className="primary active"
+            color0btn={palette.text.disabled}
+          >
             Next
           </Button>
         </Box>
-      </Box> */}
+      </Box>
     </Panel>
   )
 }
