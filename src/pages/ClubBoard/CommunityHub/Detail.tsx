1255563834f8323376e25589b37d1e8ac594330c
chris bishop <ychrisbishop@gmail.com>
2022-09-13 21:32:35 +0200

    Clone profile edit page for community edit page

--- src/pages/ClubBoard/CommunityHub/Detail.tsx
+++ src/pages/ClubBoard/CommunityHub/Detail.tsx
@@ -8,8 +8,11 @@ import { AText, Button, SocialButtons } from '../../../components'
 import avatar1 from '../../../assets/images/avatar-7.png'
 import GroupOutlinedIcon from '@mui/icons-material/GroupOutlined'
 import badge from '../../../assets/icons/verified-icon-small.png'
-
+import editIcon from '../images/edit-icon.png'
 import { DetailContent } from './'
+import { useAppDispatch } from '../../../hooks'
+import { setCurrentBoardTab } from '../../../stores/UiStore'
+import { TabID } from '../ClubBoard'
 
 export const Detail = ({
   community,
@@ -20,6 +23,8 @@ export const Detail = ({
   handleJoin: AnyFunction
   joined: boolean
 }) => {
+  const dispatch = useAppDispatch()
+  
   const Text = styled(Typography)(() => ({
     color: `${palette.text.primary}`,
     fontFamily: 'Andale Mono Regular',
@@ -29,6 +34,10 @@ export const Detail = ({
     letterSpacing: '-0.05em',
     lineHeight: '120%',
   }))
+
+  const handleEdit = () => {
+    dispatch(setCurrentBoardTab(TabID.EDIT_COMMUNITY))
+  }
   return (
     <Grid container spacing={2}>
       <Grid item lg={5} position="relative">
@@ -76,7 +85,7 @@ export const Detail = ({
         </Box>
       </Grid>
       <Grid item lg={7}>
-        <Box pt="24px">
+        <Box pt="24px" position="relative">
           <Box>
             <DetailContent />
           </Box>
@@ -86,6 +95,15 @@ export const Detail = ({
           <Box pt="15px">
             <SocialButtons />
           </Box>
+          <Button
+            className="outlined primary active"
+            color={palette.text.disabled}
+            sx={{ position: 'absolute', right: '24px', top: '24px' }}
+            onClick={handleEdit}
+          >
+            <img src={editIcon} alt="" width="24px" height="24px" />
+            &nbsp; edit community
+          </Button>
         </Box>
       </Grid>
     </Grid>
