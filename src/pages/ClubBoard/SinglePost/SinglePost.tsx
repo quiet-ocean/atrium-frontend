8d8f184887c0e4e11e347116866eb0637de0e1ec
chris bishop <ychrisbishop@gmail.com>
2022-09-14 17:36:49 +0200

    Clean code

--- src/pages/ClubBoard/SinglePost/SinglePost.tsx
+++ src/pages/ClubBoard/SinglePost/SinglePost.tsx
@@ -1,26 +1,28 @@
 import {
   Box,
-  FormControl,
-  InputLabel,
-  OutlinedInput,
-  InputAdornment,
-  IconButton,
+  // FormControl,
+  // InputLabel,
+  // OutlinedInput,
+  // InputAdornment,
+  // IconButton,
+  Typography,
 } from '@mui/material'
 import { useEffect, useState } from 'react'
 
+import { Button, Banner } from '../../../components'
 import { useAppSelector } from '../../../hooks'
 import type { IComment, IPost } from '../../../types/model'
 import { apiUrl, apiGetRequest, apiPostRequest } from '../../../utils/axios'
-import icon2 from '../images/fa-solid_search-1.png'
-import icon1 from '../images/fa-solid_search.png'
+// import icon2 from '../images/fa-solid_search-1.png'
+// import icon1 from '../images/fa-solid_search.png'
 import bg from '../images/profile-landing-image.png'
 import profileImage from '../images/Rectangle 121.png'
 import cardImage from '../images/Rectangle 138.png'
-import icon3 from '../images/Vector.png'
-import { Comment, Comments } from '../Post/Comments'
+// import icon3 from '../images/Vector.png'
+import { Comments } from '../Post/Comments'
 import * as PContainer from '../styled'
 
-import { Text, Heading, SubHead, HeadButton, Container, Card } from './styled'
+import { Text, SubHead, HeadButton, Container, Card } from './styled'
 
 const SinglePost = () => {
   const data = useAppSelector((state) => state.app.currentPost)
@@ -75,36 +77,38 @@ const SinglePost = () => {
 
   return (
     <PContainer.Main>
-      <Box>
-        <Box>
-          <img src={bg} alt="" width="100%" />
-        </Box>
-      </Box>
+      <Banner img={bg} />
       <Box>
         <Box sx={{ padding: '0px 180px' }}>
           <Box>
             <Box sx={{ p: '32px' }}>
-              <Heading>{data?.title}</Heading>
-              <Box sx={{ display: 'flex', gap: '12px', padding: '36px 0px' }}>
-                <HeadButton>spotify</HeadButton>
-                <HeadButton>atrium</HeadButton>
-                <HeadButton>trending news</HeadButton>
-                <HeadButton>integrations</HeadButton>
+              <Typography
+                variant="h1"
+                sx={{ fontSize: 60, textTransform: 'capitalize' }}
+              >
+                {data?.title}
+              </Typography>
+              <Box
+                sx={{
+                  '& button': {
+                    fontFamily: 'Andale Mono Regular',
+                    fontWeight: 100,
+                    textTransform: 'uppercase',
+                  },
+                  display: 'flex',
+                  gap: '12px',
+                  padding: '36px 0px',
+                }}
+              >
+                <Button className="primary active">spotify</Button>
+                <Button className="primary active">atrium</Button>
+                <Button className="primary active">trending news</Button>
+                <Button className="primary active">integrations</Button>
               </Box>
             </Box>
             <Box></Box>
           </Box>
           <Container>
-            {/* <SubHead>“spotify integrating with atrium”</SubHead> */}
-            {/* <Text>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Turpis eu
-              sed et tortor proin. Ac vulputate eget sagittis amet metus feugiat
-              vitae. Velit nunc, augue felis interdum integer aliquet commodo
-              vel ultrices. Feugiat malesuada tempor euismod et nibh ac laoreet
-              urna, cursus. Feugiat nibh non amet, nunc risus faucibus viverra
-              hendrerit. Cursus sed est tellus lorem nec vel. Lacinia ut rhoncus
-              massa id turpis quisque amet, non.
-            </Text> */}
             <img src={profileImage} alt="" />
             <Text>{data?.body}</Text>
           </Container>
@@ -114,50 +118,10 @@ const SinglePost = () => {
               <HeadButton>See All</HeadButton>
             </Box>
             <Box>
-              {
-                post && post.comments && post.comments.length > 0 && (
-                  <Comments
-                    data={post.comments}
-                    createComment={createComment}
-                  />
-                )
-                // post?.comments.map((comment: IComment, key: number) => {
-                //   return <Comment data={comment} key={key} createComment={createComment} />
-                // })
-              }
+              {post && post.comments && post.comments.length > 0 && (
+                <Comments data={post.comments} createComment={createComment} preview={false}/>
+              )}
             </Box>
-            {/* <Box sx={{ display: 'flex', gap: '24px', padding: '24px 0px' }}>
-              <Card sx={{ padding: '24px' }}>
-                <img src={icon1} alt="" />
-              </Card>
-              <Card sx={{ padding: '24px' }}>
-                <img src={icon2} alt="" />
-              </Card>
-              <FormControl sx={{ width: '100%' }} variant="outlined">
-                <InputLabel
-                  htmlFor="outlined-adornment-password"
-                  sx={{ fontSize: '36px', left: '12px', top: '2px' }}
-                >
-                  Type Here...
-                </InputLabel>
-                <OutlinedInput
-                  id="outlined-adornment-password"
-                  type="text"
-                  sx={{ fontSize: '42px', height: '100%' }}
-                  endAdornment={
-                    <InputAdornment position="end">
-                      <IconButton
-                        aria-label="toggle password visibility"
-                        edge="end"
-                      >
-                        <img src={icon3} alt="" />
-                      </IconButton>
-                    </InputAdornment>
-                  }
-                  label="Password"
-                />
-              </FormControl>
-            </Box> */}
           </Container>
           <Container>
             <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
