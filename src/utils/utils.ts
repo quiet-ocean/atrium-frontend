e8184a9e9a75a61f703fccfab5fb1135b7f2eb16
chris bishop <ychrisbishop@gmail.com>
2022-09-16 18:01:29 +0200

    Add url validate function

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -22,9 +22,18 @@ export const convert2LongDate = (s: string) => {
   const d = new Date(s)
   const month = MONTHS[d.getMonth()]
   const _date = d.getDate()
-  const date = _date === 1 ? _date + 'st' : _date === 2 ? _date + 'nd' : _date + 'th'
+  const date =
+    _date === 1 ? _date + 'st' : _date === 2 ? _date + 'nd' : _date + 'th'
   return month + ' ' + date + ', ' + d.getFullYear()
 }
 export const calculatePastTime = (date: string | undefined) => {
   return date
+}
+
+export const isValidUrl = (urlString) => {
+  try {
+    return Boolean(new URL(urlString))
+  } catch (e) {
+    return false
+  }
 }
