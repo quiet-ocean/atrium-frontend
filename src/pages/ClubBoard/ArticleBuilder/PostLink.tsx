88c2d4c3571db8e10a452b07e53ab78a76563b49
chris bishop <ychrisbishop@gmail.com>
2022-09-21 14:57:53 +0200

    Update PostLink.tsx

--- src/pages/ClubBoard/ArticleBuilder/PostLink.tsx
+++ src/pages/ClubBoard/ArticleBuilder/PostLink.tsx
@@ -4,6 +4,7 @@ import { getLinkPreview } from 'link-preview-js'
 import { useEffect } from 'react'
 
 import { isValidUrl } from '../../../utils'
+import axios from 'axios'
 
 import type { TPostContent } from './ArticleBuilder'
 
@@ -23,19 +24,51 @@ export const PostLink = ({
   }, [data])
   const getPreviewByLink = async () => {
     console.log(data.value)
-    const previewResult = await getLinkPreview(data.value as string, {
-      headers: {
-        'Access-Control-Allow-Origin': '*',
-        // 'Access-Control-Request-Method': 'GET',
-        'Access-Control-Allow-Headers': 'Authorization',
-        'Content-type': 'text/html; charset=UTF-8',
-        // 'method': 'GET',
-      },
-      timeout: 10000,
-      // mode: 'cors',
-    })
+    // const previewResult = await getLinkPreview(data.value as string, {
+    //   headers: {
+    //     'Access-Control-Allow-Origin': '*',
+    //     // 'Access-Control-Request-Method': 'GET',
+    //     'Access-Control-Allow-Headers': 'Authorization',
+    //     'Content-type': 'text/html; charset=UTF-8',
+    //     // 'method': 'GET',
+    //   },
+    //   timeout: 10000,
+    //   // mode: 'cors',
+    // })
+    try {
+      // const res = await axios.get(data.value as string, {
+      //   headers: {
+      //     'Access-Control-Allow-Origin': '*',
+      //   } 
+      // })
+      // const response = await axios({
+      //   url: data.value as string,
+      //   method: 'GET',
+      //   headers: {
+      //     'Access-Control-Allow-Origin': '*',
+      //     // 'Access-Control-Allow-Origin': 'http://localhost:3000',
+      //     'Access-Control-Allow-Methods': 'GET, OPTIONS, POST, PUT',
+      //     'Content-type': 'text/html; charset=UTF-8',
+      //     "Access-Control-Allow-Headers": "X-Token",
+      //     "Access-Control-Allow-Credentials": "true",
+      //   }
+      // })
 
-    console.log('preview result ', previewResult)
+      const xhttp = new XMLHttpRequest()
+
+      xhttp.onload = function() {
+        console.log(this.responseText)
+      }
+      xhttp.onerror = function (e) {
+        console.log('Error occurred while load preview link: ', e)
+      }
+      xhttp.open('GET', 'https://medium.com/coinmonks/after-the-ethereum-merge-a-post-op-2e158387eb74')
+      xhttp.send()
+      // console.log(response)
+    } catch (e) {
+      console.log('error: ', e)
+    }
+    // console.log('preview result ', previewResult)
   }
   return (
     <>
